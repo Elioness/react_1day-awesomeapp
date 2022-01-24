b880e242d3784c1a6ce0db964c922ba3c0e1420f
Elioness <lmeraki@yahoo.com>
2022-01-24 14:44:30 +0100

    Modified something to check netlify

--- src/App.js
+++ src/App.js
@@ -6,17 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          This week, I will be learning about react and thinking in components.
-        </a>
+        <h1>Eli Codes Javascript</h1>
       </header>
     </div>
   );
